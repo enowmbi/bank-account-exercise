6ea1046f738c542fb2b5948ae453e283286b0a8e
Enow Mbi <benowmbi@yahoo.com>
2021-11-17 05:29:19 +0100

    Fix typo

--- bank_account.rb
+++ bank_account.rb
@@ -4,10 +4,10 @@
 class BankAccount
   attr_reader :account_number, :account_type, :balance, :client_name
 
-  ACCOUNTTYPES = {checking: 0, saving: 1}
+  ACCOUNT_TYPES = {checking: 0, saving: 1}
 
-  def initialize(account_type: ACCOUNTINTTYPES[:checking], initial_deposit: 0, client_name:)
-    @account_type = account_type
+  def initialize(account_type: ACCOUNT_TYPES[:checking], initial_deposit: 0, client_name:)
+    @account_type = generated_account_type
     @balance = initial_deposit
     @client_name = client_name
     @account_number = account_number
@@ -15,8 +15,7 @@ class BankAccount
 
   private
 
-  def account_number
+  def generated_account_number
     rand(10).to_s[2..16]
-    # generate account number
   end
 end
